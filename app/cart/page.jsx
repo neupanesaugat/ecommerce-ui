94273ff286ba36170917f3863725157c06101169
neupanesaugat <saugatneupane50@gmail.com>
2025-01-16 14:27:59 +0545

    ecommerce-ui

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,7 +1,28 @@
+'use client';
+import CartItemCard from '@/components/CartItemCard';
+import { getCartList } from '@/lib/routes/cart.routes';
+import { isBuyer } from '@/utils/check.role';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const { data, isPending } = useQuery({
+    queryKey: ['cart-list'],
+    queryFn: () => getCartList(),
+    enabled: isBuyer(),
+  });
+
+  const cartData = data?.data?.cartData;
+
+  return (
+    <>
+      <div className=" flex  justify-center items-center flex-wrap gap-4 m-8">
+        {cartData.map((item) => {
+          return <CartItemCard key={item._id} {...item} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CartPage;
